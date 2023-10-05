7c04f6bc8839e12a42c62b531bf566cb2bd7c040
edvanflorio <edvansilva0511@gmail.com>
2023-10-04 23:34:28 -0300

    projeto API de tempo e PYTHON

---
+++ loopapi.py
@@ -0,0 +1,56 @@
+import requests
+import logging
+
+logging.basicConfig(filename='weather.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+with open('resultados.txt', 'w') as arquivo_resultados:
+
+    cidades = [
+        'macapa',
+        'santana',
+        'fazendinha',
+        'mazagão',
+        'mazagão velho',
+        'porto grande',
+        'pedra branca',
+        'amapa',
+        'lourenço',
+        'oiapoque',
+        'itaubal'
+    ]
+
+    api_key = '25f262d03d7801f1de9c542f2329c3db'
+    base_url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    for cidade in cidades:
+        params = {
+            'q': cidade,
+            'appid': api_key,
+            'units': 'metric',
+            'lang': 'pt_br'
+        }
+
+        try:
+            response = requests.post(base_url, params=params)
+            data = response.json()
+
+            if response.status_code == 200:
+                temperature = data['main']['temp']
+                weather_description = data['weather'][0]['description']
+                humidity = data['main']['humidity']
+
+                arquivo_resultados.write(f'informações meteorologicas para {cidade}:\n')
+                arquivo_resultados.write(f'Temperatura: {temperature}°C\n')
+                arquivo_resultados.write(f'Clima: {weather_description.capitalize()}\n')
+                arquivo_resultados.write(f'Umidade: {humidity}\n')
+                arquivo_resultados.write('\n')
+                
+                logging.info(f'Falha na solicitação a API {cidade}')
+            else:
+                print(f'Falha na solicitação a API para {cidade}')
+
+        except Exception as e:
+
+            logging.error(f'Erro ao obter informações meteorologicas para {cidade}: {str(e)}')
+            print(f'Erro ao obter informações meteorologicas para {cidade}: {str(e)}')
+print('Resultados salvos em resultados.txt')
---
+++ tempoapi.py
@@ -0,0 +1,24 @@
+import requests
+
+city = (input('Digite a cidade desejada: '))
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=25f262d03d7801f1de9c542f2329c3db&units=metric&lang=pt_br'
+
+
+try: 
+    response = requests.post(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        temperature = data['main']['temp']
+        weather_description = data['weather'][0]['description']
+        humidity = data['main']['humidity']
+
+        print(f'Temperatura: {temperature}°C')
+        print(f'Clima: {weather_description.capitalize()}')
+        print(f'Umidade {humidity}%')
+    else:
+        print('Falha na solicitação à API')
+
+except Exception as e:
+    print(f'Erro: {str(e)}')
